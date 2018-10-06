4424541754ba3b979273cd3c2eeee9e3b3f6c53a
Andrei Poleschuk <renardby@mail.ru>
2018-10-07 00:59:41 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,88 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.array = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.array.push('+');
+    this.array.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.array.push('-');
+    this.array.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.array.push('*');
+    this.array.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.array.push('/');
+    this.array.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.array.push('^');
+    this.array.push(number);
+    return this;
+  }
+
+  valueOf() {
+    let currentValue = 0;
+    for (let index = 0; index < this.array.length; index++) {
+      switch (this.array[index]) {
+        case '^':
+          currentValue = Math.pow(this.array[index-1],this.array[index+1]);
+          this.array.splice(index-1,3,currentValue);
+          index = 0;          
+          break;
+
+        default:
+          break;
+      }      
+    }
+    for (let index = 0; index < this.array.length; index++) {
+      switch (this.array[index]) {
+        case '/':
+          currentValue = this.array[index-1]/this.array[index+1];
+          this.array.splice(index-1,3,currentValue);
+          index = 0;
+          break;
+        case '*':
+          currentValue = this.array[index-1]*this.array[index+1];
+          this.array.splice(index-1,3,currentValue);
+          index = 0;
+          break;  
+
+        default:
+          break;
+      }
+      
+    }
+    for (let index = 0; index < this.array.length; index++) {
+      switch (this.array[index]) {
+        case '+':
+          currentValue = this.array[index-1]+this.array[index+1];
+          this.array.splice(index-1,3,currentValue);
+          index = 0;
+          break;
+        case '-':
+          currentValue = this.array[index-1]-this.array[index+1];
+          this.array.splice(index-1,3,currentValue);
+          index = 0;
+          break;
+      
+        default:
+          break;
+      }      
+    }
+    return this.array.shift();
   }
 }
 
